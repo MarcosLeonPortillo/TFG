6f66529e77c4a3a5e5e8a2b1d53fd6d45150e8a8
MarcosLeonPortillo <markos.leon.p@gmail.com>
2024-04-02 17:35:06 +0200

    Hito1

--- LeonGames/admin.py
+++ LeonGames/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Usuario, Marca, Juego, Comentarios_juegos, Pedido, Pedido_juego
 
 # Register your models here.
+
+admin.site.register(Usuario)
+admin.site.register(Marca)
+admin.site.register(Juego)
+admin.site.register(Comentarios_juegos)
+admin.site.register(Pedido)
+admin.site.register(Pedido_juego)
--- LeonGames/models.py
+++ LeonGames/models.py
@@ -1,3 +1,59 @@
 from django.db import models
 
-# Create your models here.
+class Usuario(models.Model):
+    Nombre = models.CharField(max_length=255)
+    CorreoElectronico = models.CharField(max_length=255, unique=True)
+    Contraseña = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.Nombre
+
+class Marca(models.Model):
+    Nombre = models.CharField(max_length=30, unique=True)
+
+    def __str__(self):
+        return self.Nombre
+
+    class Meta:
+        verbose_name_plural = "Marcas"
+
+class Consola(models.Model):
+    Nombre = models.CharField(max_length=30, unique=True)
+
+    def __str__(self):
+        return self.Nombre
+
+class Juego(models.Model):
+    Titulo = models.CharField(max_length=255)
+    Genero = models.CharField(max_length=255, blank=True, null=True)
+    Descripción = models.CharField(max_length=255, blank=True, null=True)
+    Desarrollador = models.CharField(max_length=255, blank=True, null=True)
+    Marca = models.ForeignKey(Marca, on_delete=models.CASCADE)
+    Consola = models.ForeignKey(Consola, on_delete=models.CASCADE)
+    Usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.Titulo
+
+class Comentarios_juegos(models.Model):
+    Juego = models.ForeignKey(Juego, on_delete=models.CASCADE)
+    Usuario = models.ForeignKey(Usuario, on_delete=models.CASCADE)
+    Texto = models.CharField(max_length=255)
+    Fecha = models.CharField(max_length=255)
+
+class Pedido(models.Model):
+    Fecha = models.DateField()
+    Comprador = models.ForeignKey(Usuario, related_name='comprador_pedidos', on_delete=models.CASCADE)
+    Vendedor = models.ForeignKey(Usuario, related_name='vendedor_pedidos', on_delete=models.CASCADE)
+    Juego = models.ForeignKey(Juego, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Pedido ID: {self.id}"
+
+class Pedido_juego(models.Model):
+    Cantidad = models.IntegerField()
+    Juego = models.ForeignKey(Juego, on_delete=models.CASCADE)
+    Pedido = models.ForeignKey(Pedido, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"Compra ID: {self.id}"
